9ff50f731a5f84746973f3213e57fdd1eb941d0d
marlontaras <141860671+marlontaras@users.noreply.github.com>
2025-09-03 14:26:58 -0300

    0309

--- script.js
+++ script.js
@@ -5,4 +5,52 @@ const checkbox = document.querySelectorAll('.checkbox');
 const botoes = document.querySelectorAll('.parametro-senha-botao');
 const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho; 
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+function geraSenha (){
+    let alfabeto = '';
+    if (checkbox[0].Checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].Checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].Checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].Checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i <tamanhoSenha; i++){
+        let numeroAleatorio = Math.random () * alfabeto.length;
+        numeroAleatorio = Math.floor (numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+}
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+    }
+}
